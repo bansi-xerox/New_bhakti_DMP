768d5be66dfa689b92c768524161261cbb8fe54c
KhushaliBhalala <khushalibhalala676@gmail.com>
2026-09-12 15:25:12 +0530

    change live url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://new-bhakti-dmp.onrender.com/api"',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://new-bhakti-dmp.onrender.com/api',
     // baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
 
   headers: { 'Content-Type': 'application/json' },
