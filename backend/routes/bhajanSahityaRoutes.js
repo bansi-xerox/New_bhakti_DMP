054ff62f17621fa184c5bcf4f8136733ca5a8182
ayushigondaliya1610 <dgondaliya209@gmail.com>
2026-09-16 21:20:42 +0530

    Add bhajans by id added

--- backend/routes/bhajanSahityaRoutes.js
+++ backend/routes/bhajanSahityaRoutes.js
@@ -10,6 +10,7 @@ router.put('/:id', protect, bhajanController.updateBhajan);
 router.delete('/:id', protect, bhajanController.deleteBhajan);
 
 router.get('/', bhajanController.getAllBhajans);
+router.get('/:id', bhajanController.getBhajanById);
 router.get('/search', bhajanController.searchBhajans); 
 
 module.exports = router;
