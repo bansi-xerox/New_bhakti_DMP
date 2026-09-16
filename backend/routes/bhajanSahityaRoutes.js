9cd9e475de530466d5d375350bd21f992d5b5ecc
Akshita-Kachhadiya <akshitakachhadiya501@gmail.com>
2026-09-16 22:04:47 +0530

    -commited search fixed completed

--- backend/routes/bhajanSahityaRoutes.js
+++ backend/routes/bhajanSahityaRoutes.js
@@ -9,10 +9,12 @@ router.post('/', protect, bhajanController.createBhajan);
 router.put('/:id', protect, bhajanController.updateBhajan);
 router.delete('/:id', protect, bhajanController.deleteBhajan);
 
-router.get('/', bhajanController.getAllBhajans);
-router.get('/:id', bhajanController.getBhajanById);
 
-router.get('/search', bhajanController.searchBhajans); 
+//search
+router.get('/', bhajanController.getAllBhajans);
+
+router.get('/search', bhajanController.searchBhajans);
+
 router.get('/:id', bhajanController.getBhajanById);
 
 
